e67c9b14b10e3185c8cd530e5644d0c8b2b966ee
Abhimanyu Haralukallu <32696360+AbhimanyuHK@users.noreply.github.com>
2023-01-24 15:22:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import pafy
+# import pafy
 
-import vlc
+# import vlc
 
+# url = "https://www.youtube.com/watch?v=OCzrpR7Jtsk"
+# video = pafy.new(url)
+# best = video.getbest()
+# playurl = best.url
+
+
+# Instance = vlc.Instance()
+# player = Instance.media_player_new()
+# Media = Instance.media_new(playurl)
+# Media.get_mrl()
+# player.set_media(Media)
+# player.audio_set_mute(True)
+# player.play()
+
+from youtube_video_play_pause_bot import *
 url = "https://www.youtube.com/watch?v=OCzrpR7Jtsk"
-video = pafy.new(url)
-best = video.getbest()
-playurl = best.url
-
-
-Instance = vlc.Instance()
-player = Instance.media_player_new()
-Media = Instance.media_new(playurl)
-Media.get_mrl()
-player.set_media(Media)
-player.audio_set_mute(True)
-player.play()
+youtube.open(url)
+youtube.play_pause_video()
